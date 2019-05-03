7ac4bfb6af3b9abd5234d63f992ecda7faf39d24
zjbren <50122261+zjbren@users.noreply.github.com>
2019-05-04 01:22:23 +1200

    First Update

---
+++ Assign2/Assign2-war/web/login.jsp
@@ -0,0 +1,33 @@
+<%-- 
+    Document   : login
+    Created on : 3/05/2019, 2:25:11 PM
+    Author     : Zach
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href=" style.css" />
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Login Page</title>
+    </head>
+    <body>
+        <h1 class="h1">Login to ZachBook</h1>
+
+        <div class ="content">
+            <%
+                if (!(request.getAttribute("error") == null)) {
+                    out.print(request.getAttribute("error"));
+                }
+            %>
+            <form method ="POST" action ="$LoginProcess">
+                User Name: <input type="test" name="userName" size ="8" required>
+                Password:  <input type="password" name ="pwd" required><br>
+                <input type="submit" value="Login" />
+            </form>
+            <button onclick="location.href = 'register.jsp'" type="button">Sign up to ZachBook</button>
+            <button onclick="location.href = 'index.jsp'" type="button">Return to Home</button>
+        </div>
+    </body>
+</html>
---
+++ DMSAssign2/DMSAssign2-ejb/src/java/Beans/UserBean.java
@@ -0,0 +1,138 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Beans;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ * Bean used to add new users to DB Validates usernames and confirms does not
+ * already exist
+ *
+ * @author Zach
+ */
+@Stateless
+@LocalBean
+public class UserBean {
+
+    public void registerUser(String usrName, String pwd, String email) {
+        
+        String driverURL = "org.apache.derby.jdbc.EmbeddedDriver";
+        // The dbURL to contain the Database URL
+        String dbURL = "jdbc:derby://localhost:1527/ZACHBOOKDB;"
+                + "create=true;user=db;password=db";
+        try
+        {
+            // Creating SQL query string
+            String sqlQuery;
+            ResultSet rs;
+            // Step 1: Loading the drivers for JAVA DB
+            Class.forName(driverURL);
+
+            Connection connection = DriverManager.getConnection(dbURL);
+
+            // Creating the SQL Statement
+            Statement statement = connection.createStatement();
+
+            // Inserting a record in the User table in the DB
+            sqlQuery = "INSET INTO USERS VALUES('userName','pwd','email') = '" + usrName + "'," + pwd + "'," + email + "'";
+            statement.executeUpdate(sqlQuery);
+
+        } catch (SQLException e)
+        {
+            System.out.println("SQL error: " + e.getMessage());
+        } catch (ClassNotFoundException e)
+        {
+            System.out.println("Class not found " + e.getMessage());
+        }
+    }
+    
+    public ArrayList<String> getUserNames()
+    {
+        ArrayList<String> userNames = new ArrayList();
+        
+        String driverURL = "org.apache.derby.jdbc.EmbeddedDriver";
+        // The dbURL to contain the Database URL
+        String dbURL = "jdbc:derby://localhost:1527/ZACHBOOKDB;"
+                + "create=true;user=db;password=db";
+        
+        try
+        {
+            // Creating SQL query string
+            String sqlQuery;
+            ResultSet rs;
+            // Step 1: Loading the drivers for JAVA DB
+            Class.forName(driverURL);
+
+            Connection connection = DriverManager.getConnection(dbURL);
+
+            // Creating the SQL Statement
+            Statement statement = connection.createStatement();
+
+            // Inserting a record in the User table in the DB
+            sqlQuery = "SELECT * FROM USERS";
+            rs = statement.executeQuery(sqlQuery);
+            
+            while(rs.next())
+            {
+                userNames.add(rs.getString("username"));
+            }
+
+        } catch (SQLException e)
+        {
+            System.out.println("SQL error: " + e.getMessage());
+        } catch (ClassNotFoundException e)
+        {
+            System.out.println("Class not found " + e.getMessage());
+        }
+        return userNames;
+    }
+    
+    public String getPwd(String userName)
+    {
+        String pwd = null;
+        
+        String driverURL = "org.apache.derby.jdbc.EmbeddedDriver";
+        // The dbURL to contain the Database URL
+        String dbURL = "jdbc:derby://localhost:1527/ZACHBOOKDB;"
+                + "create=true;user=db;password=db";
+        try
+        {
+            // Creating SQL query string
+            String sqlQuery;
+            ResultSet rs;
+            // Step 1: Loading the drivers for JAVA DB
+            Class.forName(driverURL);
+
+            Connection connection = DriverManager.getConnection(dbURL);
+
+            // Creating the SQL Statement
+            Statement statement = connection.createStatement();
+
+            // Inserting a record in the dUser table in the DB
+            sqlQuery = "Select pwd From USERS where userName = '" + userName +"'";
+            rs = statement.executeQuery(sqlQuery);
+            
+            pwd = rs.getString("pwd");
+        } catch (SQLException e)
+        {
+            System.out.println("SQL error: " + e.getMessage());
+        } catch (ClassNotFoundException e)
+        {
+            System.out.println("Class not found " + e.getMessage());
+        }
+        return pwd;
+    }
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
